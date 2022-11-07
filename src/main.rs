b75702b5d4c82cd90766218f2d3f7549dc0017e4
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2022-11-07 20:36:04 +0800

    Add plotting and chance analysis

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::io;
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
-use std::ops::Deref;
 use chrono::prelude::*;
+use plotters::prelude::*;
 
 struct Pull {
     special: bool,
+    win_fifty: bool,
     pities: i32,
     time: NaiveDateTime,
 }
@@ -14,7 +15,9 @@ struct Pull {
 #[derive(Copy, Clone)]
 struct Avg {
     avg: f64,
-    quantity: i32,
+    win_chance: f64,
+    special_count: i32,
+    count: i32,
 }
 
 fn main() {
@@ -32,9 +35,11 @@ fn main() {
         let mut iter = reader.lines();
         iter.next();
         let mut pities = 0;
+        let mut win_fifty_flag = true;
+
         for line in iter {
-            let unwraped_line = line.unwrap();
-            let strings = unwraped_line.split(',').collect::<Vec<&str>>();
+            let unwrapped_line = line.unwrap();
+            let strings = unwrapped_line.split(',').collect::<Vec<&str>>();
 
             match strings[1].as_ref() {
                 "100" | "200" | "302" => continue,
@@ -48,18 +53,20 @@ fn main() {
                 _ => ()
             }
 
-            // println!("Collected 5-star {} with pity {}", strings[0], pities);
+            let special = !non_special.contains(&strings[0]);
             array.push(Pull {
-                special: non_special.contains(&strings[0]),
+                special,
+                win_fifty: win_fifty_flag,
                 pities,
                 time: NaiveDateTime::parse_from_str(strings[5].as_ref(), "%Y-%m-%d %H:%M:%S").unwrap(),
             });
 
+            win_fifty_flag = special;
             pities = 0;
         }
     }
 
-    let mut time_stats = [Avg { avg: 0.0, quantity: 0 }; 48];
+    let mut time_stats = [Avg { avg: 0.0, win_chance: 0.0, special_count: 0, count: 0 }; 49];
     let mut out = File::create("D:\\result.txt").unwrap();
     for entry in array.iter() {
         // out.write(format!("{},{},{}\n", entry.special, entry.pities, entry.time.to_string()).as_ref()).expect("TODO: panic message");
@@ -67,9 +74,54 @@ fn main() {
         let time = entry.time.time();
         let index: usize = (2 * time.hour() + if time.minute() >= 30 { 1 } else { 0 }) as usize;
         let mut avg_ref: &mut Avg = time_stats.get_mut(index).unwrap();
-        avg_ref.avg = (avg_ref.avg * avg_ref.quantity as f64 + entry.pities as f64) / (avg_ref.quantity as f64 + 1.0);
-        avg_ref.quantity += 1;
+        avg_ref.avg = (avg_ref.avg * avg_ref.count as f64 + entry.pities as f64) / (avg_ref.count as f64 + 1.0);
+        avg_ref.count += 1;
+
+        if entry.special {
+            avg_ref.win_chance = (avg_ref.win_chance * avg_ref.special_count as f64 + if entry.win_fifty { 1.0 } else { 0.0 }) / (avg_ref.special_count as f64 + 1.0);
+            avg_ref.special_count += 1;
+        }
     }
+
+    time_stats[48] = time_stats[0];
+
+    let root_area = BitMapBackend::new("D:\\test.png", (1000, 500)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40.0)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40.0)
+        .set_label_area_size(LabelAreaPosition::Right, 40.0)
+        .set_label_area_size(LabelAreaPosition::Top, 40.0)
+        .caption("Genshin Avg Pulls", ("sans-serif", 40.0))
+        .build_cartesian_2d(0.0..24.0, 55.0..65.0).unwrap()
+        .set_secondary_coord(0.0..24.0, 0.4..0.6);
+
+    ctx
+        .configure_mesh()
+        .bold_line_style(&WHITE.mix(0.3))
+        .y_desc("Pulls")
+        .x_desc("Time")
+        .x_labels(25)
+        .axis_desc_style(("sans-serif", 15))
+        .draw().expect("TODO: panic message");
+
+    ctx
+        .configure_secondary_axes()
+        .y_desc("Chance of winning fifty")
+        .draw().expect("TODO: panic message");
+
+    ctx.draw_series(
+        LineSeries::new((0..49).zip(time_stats.iter()).map(|(x, y)| {
+            ((x as f64) / 2.0, y.avg)
+        }), &BLUE)
+    ).unwrap();
+
+    ctx.draw_secondary_series(
+        LineSeries::new((0..49).zip(time_stats.iter()).map(|(x, y)| {
+            ((x as f64) / 2.0, y.win_chance)
+        }), &RED)
+    ).unwrap();
 
     for (i, el) in time_stats.iter().enumerate() {
         let hour = i / 2;
@@ -78,7 +130,7 @@ fn main() {
         let str = format!("{0:>0width$}:{1:>0width$} to {2:>0width$}:{3:>0width$} q={4} avg = {5}\n",
                           hour, minute_start,
                           if minute_end == 60 { hour + 1 } else { hour }, minute_end % 60,
-                          el.quantity, el.avg, width=2
+                          el.count, el.avg, width = 2
         );
         out.write(str.as_bytes()).expect("TODO: panic message");
     }
